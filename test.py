2727b0a04ce6c4ffb1dba12ef69556d02b4332b4
M Data <maatherdata@gmail.com>
2025-04-14 09:53:57 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-![Image Alt](https://github.com/MaatherData/MaatherData/blob/dd33180b02dc2408e16b3f42722b0ad59b0e475d/IMG-20250413-WA0042.jpg)
-
-
-
-
 
 input_text = input("Enter candle heights (space separated): ")
 
